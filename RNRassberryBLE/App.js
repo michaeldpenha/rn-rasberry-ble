b5652812cfb01315f334d59ecc7ed7261be95451
Michael Dpenha <michael.dpenha1988@gmail.com>
2020-12-18 14:12:07 +0530

    feat: inti project setup

---
+++ RNRassberryBLE/App.js
@@ -0,0 +1,159 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TextInput,
+  Button,
+  ActivityIndicator,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {EventRegister} from 'react-native-event-listeners';
+import Rassberry from './src/components/rassberry';
+import {useEffect} from 'react';
+
+const App: () => React$Node = () => {
+  const [text, setText] = useState('');
+  const [display, setDisplay] = useState(false);
+  const [chars, setCharacters] = useState('');
+  const [showActivity, setActiivity] = useState(false);
+
+  const onBLEDeviceConnection = () => {
+    EventRegister.emit('connect_device', text);
+    setActiivity(true);
+    setText('');
+  };
+
+  const sendToBLE = () => {
+    EventRegister.emit('led_val', chars);
+  };
+
+  useEffect(() => {
+    EventRegister.addEventListener('rassberry_connect', () => {
+      showActivity(false);
+      setDisplay(true);
+    });
+
+    return () => EventRegister.removeEventListener('rassberry_connect');
+  }, []);
+
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <Rassberry />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          {showActivity ? (
+            <View style={[styles.container, styles.horizontal]}>
+              <ActivityIndicator size="large" color="#00ff00" />
+            </View>
+          ) : (
+            <View style={styles.body}>
+              {!display ? (
+                <View style={styles.sectionContainer}>
+                  <Text style={styles.sectionTitle}>Enter Device</Text>
+                  <View style={{marginTop: 10, marginBottom: 10}}>
+                    <TextInput
+                      style={{height: 40, borderWidth: 2}}
+                      placeholder="Type Device name"
+                      onChangeText={text => setText(text)}
+                      defaultValue={text}
+                    />
+                  </View>
+                  <Button
+                    title="Enter"
+                    onPress={() => onBLEDeviceConnection()}
+                  />
+                </View>
+              ) : (
+                <View style={styles.sectionContainer}>
+                  <Button
+                    title="Back"
+                    onPress={() => {
+                      setDisplay(false);
+                      setCharacters('');
+                    }}
+                  />
+                  <Text style={styles.sectionTitle}>Enter BLE String</Text>
+                  <View style={{marginTop: 10, marginBottom: 10}}>
+                    <TextInput
+                      style={{height: 40, borderWidth: 2}}
+                      placeholder="Type Characters"
+                      onChangeText={text => setCharacters(text)}
+                      defaultValue={chars}
+                    />
+                  </View>
+                  <Button title="Enter" onPress={() => sendToBLE()} />
+                </View>
+              )}
+            </View>
+          )}
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  horizontal: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10,
+  },
+});
+
+export default App;
---
+++ RNRassberryBLE/src/components/rassberry/index.js
@@ -0,0 +1,184 @@
+import React, {useEffect, useState} from 'react';
+import base64 from 'react-native-base64';
+import {Alert, Platform, PermissionsAndroid} from 'react-native';
+import {EventRegister} from 'react-native-event-listeners';
+import {BleManager} from 'react-native-ble-plx';
+
+const Rassberry = (props) => {
+  const instance = new BleManager();
+
+  const [writeCharacteristic, setWriteCharacteristic] = useState({});
+  const [caharacteristicId, setCharacteristicId] = useState('');
+  const [serviceId, setServiceID] = useState('');
+  // const [device, setDeviceConnection] = useState({});
+
+  useEffect(() => {
+    EventRegister.addEventListener('connect_device', (name) => {
+      scanAndConnect(name);
+    });
+    (async function () {
+      if (Platform.OS === 'ios') {
+        instance.onStateChange((state) => {
+          if (state === 'PoweredOn') {
+            scanAndConnect();
+          }
+        }, true);
+      } else if (Platform.OS === 'android') {
+        const isPermissionGrant = await PermissionsAndroid.check(
+          PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
+        );
+
+        if (isPermissionGrant) {
+          scanAndConnect();
+        } else {
+          const permissionRequest = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
+            {
+              title: 'Location Permission',
+              message: 'QuickPick App needs access to your location.',
+              buttonNegative: 'Cancel',
+              buttonPositive: 'OK',
+            },
+          );
+
+          permissionRequest === PermissionsAndroid.RESULTS.GRANTED
+            ? scanAndConnect()
+            : null;
+        }
+      }
+    })();
+
+    return () => {
+      EventRegister.removeEventListener('led_val');
+      EventRegister.removeEventListener('rassberry_disconnect');
+      EventRegister.removeEventListener('connect_device');
+      EventRegister.removeEventListener('rassberry_connect');
+    };
+  }, []);
+
+  const disconnectRasberry = async (connectedDevice) => {
+    if (Object.keys(connectedDevice).length !== 0) {
+      await connectedDevice?.cancelConnection();
+    }
+  };
+
+  const setServicesFromDevice = (device) => {
+    device.services().then((services) => {
+      if (services && services.length) {
+        //set services in state
+        let Services = [];
+        services.map((val, key) => {
+          return (Services[val.uuid] = val);
+        });
+        //set Characteristics
+        setCharacteristicsFromService(device, Services);
+      }
+    });
+  };
+  const setCharacteristicsFromService = (device, services) => {
+    const promises = [];
+
+    Object.keys(services).map((uuid, key) => {
+      promises.push(
+        device.characteristicsForService(uuid).then((characteristics) => {
+          if (characteristics && characteristics.length) {
+            characteristics.map((val, key) => {
+              const {
+                isWritableWithResponse,
+                isWritableWithoutResponse,
+                uuid: characteristicId,
+              } = val;
+
+              if (isWritableWithResponse || isWritableWithoutResponse) {
+                setWriteCharacteristic(characteristics);
+                setCharacteristicId(characteristicId);
+                setServiceID(uuid);
+                EventRegister.addEventListener('led_val', (result) => {
+                  instance.writeCharacteristicWithoutResponseForDevice(
+                    device.id,
+                    uuid,
+                    characteristicId,
+                    base64.encode(result),
+                  );
+                });
+
+                // instance.writeCharacteristicWithoutResponseForDevice(
+                //   device.id,
+                //   uuid,
+                //   characteristicId,
+                //   base64.encode(ledVal),
+                // );
+              }
+            });
+          }
+        }),
+      );
+    });
+    setDevice(device);
+  };
+  const setDevice = (device) => {
+    //setConnectedDevice(device);
+    // dispatch({
+    //   type: 'rasBleManager',
+    //   rasBleManager: {
+    //     instance,
+    //     device,
+    //   },
+    // });
+    // setDeviceConnection(device);
+  };
+
+  const deviceConnection = (device) => {
+    device
+      .connect()
+      .then((device) => {
+        return device.discoverAllServicesAndCharacteristics();
+      })
+      .then((device) => {
+        Alert.alert('Rasberry Device connected');
+        // Do work on device with services and characteristics
+        setServicesFromDevice(device);
+        EventRegister.emit('rassberry_connect');
+        // EventRegister.addEventListener('led_val', (result: string) => {
+        //   writeToDevice(result, device);
+        // });
+        EventRegister.addEventListener('rassberry_disconnect', () => {
+          disconnectRasberry(device);
+        });
+      })
+      .catch((error) => {
+        // Handle errors
+      });
+  };
+  const scanAndConnect = async (deviceName) => {
+    if(!deviceName){
+      return;
+    }
+    // if (Object.keys(device).length !== 0) {
+    //   return;
+    // }
+
+    instance.startDeviceScan(null, null, (error, device) => {
+      if (error) {
+        // Handle error (scanning will be stopped automatically)
+        return;
+      }
+      console.log('Rasberry BLE Connection', deviceName);
+
+      if (device?.name?.replace(/\s/g, '').includes(deviceName.trim())) {
+        instance.stopDeviceScan();
+        deviceConnection(device);
+        device.onDisconnected(() => {
+          console.log('Disconnected Rasberry');
+          setDevice({});
+          EventRegister.removeEventListener('led_val');
+          EventRegister.removeEventListener('rassberry_disconnect');
+        });
+      }
+    });
+  };
+
+  return <></>;
+};
+
+export default Rassberry;
